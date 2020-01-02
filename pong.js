b0f0c1fa3b00941bb4d1079c378f9b1db71d9494
MMarek <mildnermarek@gmail.com>
2020-01-02 11:43:39 +0100

    circle & something text

--- pong.js
+++ pong.js
@@ -12,3 +12,21 @@ drawRect(0, 0, cvs.width, cvs.height, "black");
 
 // tworzenie koła
 
+function drawCircle(x,y,r,color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x,y,r,0,Math.PI*2,false);
+    ctx.fill();
+}
+
+drawCircle(100,100,50,"WHITE");
+
+// text
+
+function drawText(text,x,y,color) {
+    ctx.fillStyle = color;
+    ctx.font = "45px fantasy";
+    ctx.fillText(text,x,y);
+}
+
+drawText("something",300,200, "WHITE");
