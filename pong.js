3c645df1e8fdfd8ad96b17500f4df0e912513872
MMarek <mildnermarek@gmail.com>
2020-01-02 12:18:27 +0100

    paletki, funkcje gry, render, punktacja

--- pong.js
+++ pong.js
@@ -2,13 +2,46 @@
 const cvs = document.getElementById("pong");
 const ctx = cvs.getContext("2d");
 
+// tworzenie paletek/rakiet gracza
+
+const user = {
+    x : 0,
+    y : cvs.height/2 - 100/2,
+    width : 10,
+    height : 100,
+    color : "WHITE",
+    score : 0
+};
+
+// tworzenie paletek/rakiet komputera
+
+const com = {
+    x : cvs.width - 10,
+    y : cvs.height/2 - 100/2,
+    width : 10,
+    height : 100,
+    color : "WHITE",
+    score : 0
+};
+
+// tworzenie piłeczki
+
+const ball = {
+    x : cvs.width/2,
+    y : cvs.height/2,
+    radius : 10,
+    speed : 5,
+    velocityX : 5,
+    velocityY : 5,
+    color : "WHITE"
+};
+
 // rysowanie funkcji - przestrzeń robocza
 function drawRect(x, y, w, h, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x,y,w,h);
 }
 
-drawRect(0, 0, cvs.width, cvs.height, "black");
 
 // tworzenie koła
 
@@ -30,3 +63,25 @@ function drawText(text,x,y,color) {
 }
 
 drawText("something",300,200, "WHITE");
+
+
+// render Gry
+
+function  render() {
+    // czyszczenie parapetrów/canvas
+    drawRect(0, 0, cvs.width, cvs.height, "black");
+
+    // siatka na stole
+    drawNet();
+
+    // punktacja
+drawText(user.score,cvs.width/4,cvs.height/5,"WHITE");
+drawText(com.score,3*cvs.width/4,cvs.height/5,"WHITE");
+
+// paletki gracza i komputera
+    drawRect(user.x, user.y, user.width, user.height, user.color);
+    drawRect(com.x, com.y, com.width, com.height, com.color);
+
+    // piłeczka
+    drawCircle(ball.x, ball.y, ball.radius, ball.color);
+}
